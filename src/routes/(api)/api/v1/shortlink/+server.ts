46eb020efbf3d43bf17ed643a5da4992132c6165
Patsagorn <patsagorn_y@outlook.com>
2023-09-04 13:52:07 +0700

    update

--- src/routes/(api)/api/v1/shortlink/+server.ts
+++ src/routes/(api)/api/v1/shortlink/+server.ts
@@ -1,25 +1,25 @@
-import { admin } from '$lib/server/firebase';
-import { error, json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-import { getFirebaseTokenFromRequest } from '$lib/server/utils';
+// import { admin } from '$lib/server/firebase';
+// import { error, json } from '@sveltejs/kit';
+// import type { RequestHandler } from './$types';
+// import { getFirebaseTokenFromRequest } from '$lib/server/utils';
 
 /**
  * create a new dynamic link
  */
-export const POST: RequestHandler = async ({ request }) => {
-	const firebaseToken = getFirebaseTokenFromRequest(request);
-	const { link, domain } = await request.json();
-	console.table({ link, domain });
-	// if (!link) {
-	// 	throw error(400, 'missing link');
-	// }
+// export const POST: RequestHandler = async ({ request }) => {
+// 	const firebaseToken = getFirebaseTokenFromRequest(request);
+// 	const { link, domain } = await request.json();
+// 	console.table({ link, domain });
+// 	// if (!link) {
+// 	// 	throw error(400, 'missing link');
+// 	// }
 
-	admin.auth().verifyIdToken(firebaseToken).then((decodedToken) => {
-		const uid = decodedToken.uid;
-		console.log({ uid });
-	}).catch((error) => {
-		console.log({ error });
-	});
+// 	admin.auth().verifyIdToken(firebaseToken).then((decodedToken) => {
+// 		const uid = decodedToken.uid;
+// 		console.log({ uid });
+// 	}).catch((error) => {
+// 		console.log({ error });
+// 	});
 
-	return json({ link, domain });
-};
+// 	return json({ link, domain });
+// };
